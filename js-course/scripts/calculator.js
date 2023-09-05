f1cb0f4de0ebdc20bd04f53d85cb6d9e4cac22c7
Lewis Bentley <ldb2003@sky.com>
2023-09-06 00:38:16 +0100

    done lesson 10 js

---
+++ js-course/scripts/calculator.js
@@ -0,0 +1,14 @@
+let calculation = localStorage.getItem("calculation") || "";
+        document.querySelector(".calculation").innerHTML = calculation;
+        
+        function updateCalculation(value) {
+            calculation += value;
+            document.querySelector(".calculation").innerHTML = calculation;
+            localStorage.setItem("calculation", calculation);
+            }
+
+            // Optional: you can also create a function in order
+            // to reuse this code.
+            function saveCalculation() {
+                localStorage.setItem("calculation", calculation);
+            }
