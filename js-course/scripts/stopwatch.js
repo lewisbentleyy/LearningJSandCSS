9fe8a27f8122ca564072966bf2c0d232693ec275
Lewis Bentley” <ldb2003@sky.com>
2023-09-12 16:41:57 +0100

    laptop work

--- js-course/scripts/stopwatch.js
+++ js-course/scripts/stopwatch.js
@@ -1,13 +1,49 @@
-document.querySelector('.js-reset').addEventListener('click', () => {
-    document.querySelector('.js-time').innerHTML = '00:00.00'
-})
-
-document.querySelector('.js-start').addEventListener('click', () => {
-
-})
-
 let intervalID;
 
+document.querySelector(".js-reset").addEventListener("click", () => {
+    document.querySelector(".js-min").innerHTML = "00";
+    document.querySelector(".js-sec").innerHTML = "00";
+    document.querySelector(".js-millisec").innerHTML = "00";
+    clearInterval(intervalID);
+    let minute = 0;
+    let second = 0;
+    let millisec = 0;
+});
+
+document.querySelector(".js-start").addEventListener("click", () => {
+    start();
+
+});
+
+let minute = 0;
+let second = 0;
+let millisec = 0;
+
 function start() {
-    intervalID = setInterval()
+    intervalID = setInterval(() => {
+        millisec++;
+        if (millisec < 10) {
+            document.querySelector(".js-millisec").innerHTML = `0${millisec}`;
+        } else if (millisec < 100) {
+            document.querySelector(".js-millisec").innerHTML = millisec;
+        } else{
+            millisec = 0;
+            second++;
+            document.querySelector(".js-millisec").innerHTML = '00';
+            if (second < 10){
+                document.querySelector(".js-sec").innerHTML =`0${second}`;
+            } else if (second < 60){
+                document.querySelector(".js-sec").innerHTML =`${second}`;
+            } else {
+                second = 0;
+                minute++;
+                document.querySelector(".js-sec").innerHTML =`0${second}`;
+                if (minute < 10){
+                    document.querySelector(".js-min").innerHTML =`0${minute}`;
+                } else if (second < 60){
+                    document.querySelector(".js-min").innerHTML =`${minute}`;
+                }
+            }
+        }
+    }, 10);
 }
