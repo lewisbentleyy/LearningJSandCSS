7adfdae0ffd23b6b5cb63d60ecb5cb19a7d9bd60
Lewis Bentley <ldb2003@sky.com>
2023-09-13 19:16:49 +0100

    quantity selector functionality

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -63,6 +63,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 
         let matchingItem;
         let cartQuantity = 0;
+        const quantitySelected = document.querySelector(`.js-quantity-selector-${productId}`).value
 
         cart.forEach((item) => {
             if (productId === item.productId) {
@@ -71,11 +72,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         });
 
         if (matchingItem) {
-            matchingItem.quantity++;
+            matchingItem.quantity+=Number(quantitySelected);
         } else {
             cart.push({
                 productID: productId,
-                quantity: 1,
+                quantity: Number(quantitySelected),
             });
         }
 
