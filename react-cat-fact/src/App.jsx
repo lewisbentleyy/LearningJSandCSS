ea50cc0898a80b17cf9c6364d421d7b597b29d01
Lewis Bentley <ldb2003@sky.com>
2023-09-17 21:20:51 +0100

    excuse app done

--- react-cat-fact/src/App.jsx
+++ react-cat-fact/src/App.jsx
@@ -1,27 +1,40 @@
-import { useEffect } from "react";
 import "./App.css";
 import Axios from "axios";
-import { useState } from "react";
+import { useState, userEffect } from "react";
 
 function App() {
+    const [reason, setExcuse] = useState(null);
 
-    const [catFact, setCatFact] = useState("")
-    
-    const fetchCatFact = ()=>{
-        Axios.get("https://catfact.ninja/fact").then((res) =>
-            setCatFact(res.data.fact)
-        );
-    }
-
-    useEffect(()=>{
-        fetchCatFact()
-    }, [])
-
+    const generateExcuse = (category) => {
+        Axios.get(
+            `https://excuser-three.vercel.app/v1/excuse/${category}`
+        ).then((res) => {
+            setExcuse(res.data[0]);
+        });
+    };
 
     return (
         <div className="App">
-            <button onClick={fetchCatFact} className="cat-fact-button">Generate Cat Fact</button>
-            <p className="cat-fact">{catFact}</p>
+            <h1>Generate An Excuse</h1>
+            <button
+                onClick={() => {
+                    generateExcuse("party");
+                }}>
+                Party
+            </button>
+            <button
+                onClick={() => {
+                    generateExcuse("family");
+                }}>
+                Family
+            </button>
+            <button
+                onClick={() => {
+                    generateExcuse("office");
+                }}>
+                Office
+            </button>
+            <p>{reason?.excuse}</p>
         </div>
     );
 }
